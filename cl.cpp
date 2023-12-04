974dfa0b1dcef4e0ce76bb1f85d7a456cbdb8254
CuteBin1112 <twyb1227@naver.com>
2023-12-04 19:06:08 +0900

    코드수정 2번째

--- cl.cpp
+++ cl.cpp
@@ -6,10 +6,11 @@ void main(void) {
 	scanf("%d", &month);
 	if (month == 2)
 		days = 28;
-	else if (month == 4 || month == 6 || month == 9 || month == 11)
+	else if (month == 4 || month == 6 || month == 9 || month == 11 || 3)
 		days = 30;
 	else days= 31;
 	printf("%d월의 일수는 %d입니다.\n", month, days);
-	printf("안녕하세요 깃허브 입니다")
+	printf("안녕하세요 깃허브 입니다");
+	printf("코드 수정 두번째");
 	
 }
